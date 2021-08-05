f3bdb76266d466cddf0cdb074c787a14f4ccabdf
wilfridboris <boriskwayep35@yahoo.com>
2021-08-05 01:28:06 -0400

    :whale2:

--- src/Head.js
+++ src/Head.js
@@ -1,18 +1,30 @@
-import React,{Component, useState} from 'react'
+import React,{Component} from 'react'
 import './head.css'
-import {BsList ,BsGrid} from "react-icons/bs";
+import {BsGridFill ,BsGrid,BsChevronDown,BsPlus} from "react-icons/bs";
 import $ from 'jquery';
 
-export default class Head extends Component {
 
+export default class Head extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            iconIsToggle:false
+        }
+    }
 
 
     toggleItemStyle=()=>{
+        const menu1=$('.header-item')
+        const menu2=$('.header-option')
         $('.heard-bar').on('click',()=>{
-            if($('.item').is(':visible')){
-                $('.item').hide()
+            if(menu1.hasClass('select')){
+                menu1.removeClass('select')
+                menu2.removeClass('select')
+    
             }else{
-                $('.item').show()
+                menu1.addClass('select')
+                menu2.addClass('select')
+
             }
         })
     }
@@ -29,10 +41,10 @@ export default class Head extends Component {
             <div>
             <ul className="header-option">
                     <li className="logo">Logo </li>
-                    <li className="heard-bar"> <BsGrid size="1.6em" color="white"/> </li>
-                    <li className="item">Contact us</li>
-                    <li className="item">Support</li>
-                    <li className="item">English</li>
+                    <li className="heard-bar" onClick={()=>{this.setState({iconIsToggle:!this.state.iconIsToggle})}}> {this.state.iconIsToggle?<BsGridFill size="1.6em" color="white"/>:<BsGrid size="1.6em" color="white"/>} </li>
+                    <li className="item">Contact us <BsPlus className="Plus" size="1.5em" color="white"/></li>
+                    <li className="item">Support <BsPlus className="Plus" size="1.5em" color="white"/></li>
+                    <li className="item">English <BsPlus className="Plus" size="1.5em" color="white"/></li>
                    
                   
                    
@@ -43,7 +55,7 @@ export default class Head extends Component {
                 <ul className="header-item">
                     <li className="item dropdown">
                     
-                    Services 
+                    Services  <BsChevronDown className="ChevronDown"  color="white"/> <BsPlus className="Plus" size="1.5em" color="white"/>
                     <div className="Products">
                     <section className="Product-section">
                         <h2><a href="">Shopify</a></h2>
@@ -73,7 +85,7 @@ export default class Head extends Component {
             </div>
                     
                     </li>
-                    <li className="item dropdown">Solution
+                    <li className="item dropdown">Solution <BsChevronDown className="ChevronDown" color="white"/> <BsPlus className="Plus" size="1.5em" color="white"/>
                     <div className="Solution">
                     <section className="Product-section">
                         <h2><a href="">Web Management</a></h2>
@@ -95,9 +107,9 @@ export default class Head extends Component {
                
             </div>
             </li>
-                    <li className="item">Pricing</li>
-                    <li className="item">Learn</li>
-                    <li className="item">More</li>
+                    <li className="item">Pricing  <BsChevronDown className="ChevronDown" color="white"/> <BsPlus className="Plus" size="1.5em" color="white"/></li>
+                    <li className="item">Learn <BsChevronDown className="ChevronDown" color="white"/> <BsPlus className="Plus" size="1.5em" color="white"/></li>
+                    <li className="item">More <BsChevronDown className="ChevronDown" color="white"/> <BsPlus className="Plus" size="1.5em" color="white"/></li>
                   
 
                 </ul>
