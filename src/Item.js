c9c85180cbefc3acabb8d638d1bded9bd40309a8
wilfridboris <boriskwayep35@yahoo.com>
2021-08-15 11:36:46 -0400

    :whale2:

--- src/Item.js
+++ src/Item.js
@@ -6,24 +6,38 @@ export default function Item({children}) {
     
 
  
-    const [IsSubmenuToggle,setIsSubmenuToggle]= useState(false)
+    // const [IsSubmenuToggle,setIsSubmenuToggle]= useState(false)
     const [state,setState]=useState({
         mobileView:false
     });
     const {mobileView}=state;
-    const displayMobile=()=>{
-        return (
-            <li className={ IsSubmenuToggle?"item dropdown submenu-active":"item dropdown "} onClick={()=>setIsSubmenuToggle(!IsSubmenuToggle)}><a tabIndex="0">{children}</a><BsChevronDown className="ChevronDown" color="white"/> <BsPlus className="Plus" size="1.5em" color="white"/></li>
+    
+        function openSub(e){
+            if(e.target.classList.contains('submenu-active')){
+                e.target.classList.remove('submenu-active');
 
-        )
-    }
-    const displayDesktop=()=>{
-        return (
-            <li className="item dropdown"><a tabIndex="0">{children} <BsChevronDown className="ChevronDown" color="white"/></a> <BsPlus className="Plus" size="1.5em" color="white"/></li>
+            }else{
+                e.target.classList.add('submenu-active');
+            }
+           
+           
 
-        )
-    }
+        }
+    // const displayMobile=()=>{
+    //     return (
+    //         <li className="item dropdown " onClick={openSub}><a tabIndex="0">{children}</a><BsChevronDown className="ChevronDown" color="white"/> <BsPlus className="Plus" size="1.5em" color="white"/></li>
+
+    //     )
+    // }
+    // const displayDesktop=()=>{
+    //     return (
+    //         <li className="item dropdown"><a tabIndex="0">{children} <BsChevronDown className="ChevronDown" color="white"/></a> <BsPlus className="Plus" size="1.5em" color="white"/></li>
+
+    //     )
+    // }
     useEffect(()=>{
+
+
 
         const setResponsiveness=()=>{
             return window.innerWidth<600?setState((prevState)=>({...prevState,mobileView:true})):setState((prevState)=>({...prevState,mobileView:false}))
@@ -39,7 +53,7 @@ export default function Item({children}) {
    
     return (
         <>
-            {mobileView?displayMobile():displayDesktop()}
+        <li className="item dropdown " onClick={openSub}><a tabIndex="0">{children}</a><BsChevronDown className="ChevronDown" color="white"/> <BsPlus className="Plus" size="1.5em" color="white"/></li>
         </>
       
      
